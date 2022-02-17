b41cc4da69101d2de1035c266b9c97ff0dcc1c48
Marcio Rela <marciorela75@gmail.com>
2022-02-17 17:39:07 -0300

    FIAP: Aula de introdução

---
+++ fiap/fiap-web-2022/Controllers/Aluno.cs
@@ -0,0 +1,12 @@
+﻿namespace fiap_web_2022.Controllers
+{
+    public class Aluno
+    {
+        public Aluno()
+        {
+        }
+
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+}
---
+++ fiap/fiap-web-2022/Program.cs
@@ -0,0 +1,26 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default1",
+    pattern: "{controller=home}/{action=index}/{id?}"
+);
+
+app.MapControllerRoute(
+    name: "default2",
+    pattern: "{controller}/{action}/{id?}",
+    defaults: new { controller = "home", action = "index"}
+);
+
+
+
+app.Run();
+
+//app.MapGet("/", () => "Olá, boa noite");
+
+
